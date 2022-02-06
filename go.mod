9b580bd93846e9efb938e6ec1db3d39f300d511d
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2022-02-06 09:54:36 +0100

    join image functions to the helpers

--- go.mod
+++ go.mod
@@ -6,19 +6,18 @@ require (
 	github.com/google/uuid v1.3.0
 	github.com/gorilla/csrf v1.7.1
 	github.com/gorilla/mux v1.8.0
+	github.com/gorilla/sessions v1.2.1
 	github.com/jmoiron/sqlx v1.3.4
 	github.com/lib/pq v1.10.4
 	golang.org/x/image v0.0.0-20190802002840-cff245a6509b
 	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8
 )
 
-require github.com/pkg/errors v0.9.1 // indirect
-
 require (
 	cloud.google.com/go v0.65.0 // indirect
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/gorilla/securecookie v1.1.1 // indirect
-	github.com/gorilla/sessions v1.2.1
+	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
 	google.golang.org/appengine v1.6.6 // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
--- helpers.go
+++ helpers.go
@@ -5,10 +5,20 @@ import (
 	"database/sql"
 	"fmt"
 	"html/template"
+	"image"
+	_ "image/gif"
+	"image/jpeg"
+	_ "image/png"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"path"
 	"reflect"
 	"strings"
+
+	"github.com/google/uuid"
+	"golang.org/x/image/draw"
 )
 
 func Helpers() {
@@ -190,4 +200,32 @@ func can(who *User, do string, what interface{}) bool {
 		log.Fatal(err)
 	}
 	return true
+}
+
+func ImageResize(in io.Reader, out io.Writer, w, h int) error {
+	src, _, err := image.Decode(in)
+	if err != nil {
+		return err
+	}
+
+	dst := image.NewRGBA(image.Rect(0, 0, w, h))
+	draw.NearestNeighbor.Scale(dst, dst.Rect, src, src.Bounds(), draw.Over, nil)
+
+	return jpeg.Encode(out, dst, &jpeg.Options{Quality: 90})
+}
+
+func UploadImage(in io.Reader, p string, w, h int) (string, error) {
+	name := uuid.New().String()
+
+	out, err := os.Create(path.Join(p, name))
+	if err != nil {
+		return "", err
+	}
+
+	err = ImageResize(in, out, w, h)
+	if err != nil {
+		return "", err
+	}
+
+	return name, nil
 }
