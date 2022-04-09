26bc24fdd8ab44a8d65e53f11216ae0a928bc210
haithamsha <haithamshaabann@gmail.com>
2022-04-09 16:24:13 +0200

    remove sectets data

--- .env.run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export DATABASE_URL=postgres://odoo-13:odoo-13@localhost/library_development?sslmode=disable
-export SESSION_SECRET=mysessionsecret1forfirstsessionfirsttime
-export GOOGLE_CLIENT_ID=957604366444-tfje2deu51759js5l1floojl42bpo4d2.apps.googleusercontent.com
-export GOOGLE_CLIENT_SECRET=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
-export BACKUPS_PATH=/path/to/backups
-export BACKUPS_LIMIT=30
-export DOMAIN=http://127.0.0.1:3000
